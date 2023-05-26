67f7fd19f2a6aa28c1e54cae03e1368eeb67f7db
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2023-05-27 01:01:48 +0500

    yaxshi :+1:

--- backend/products/models.py
+++ backend/products/models.py
@@ -43,7 +43,8 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=0)
+    old_price = models.DecimalField(max_digits=10, decimal_places=0, default=0)
     image = models.ImageField(upload_to='products/')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
